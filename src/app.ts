b6cf0d4d35f34b86058f2234d9c3b952400ab368
Alistair Smith <hi@alistair.sh>
2023-02-03 00:41:10 +0000

    fix: zod error handle as 400

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import {createFMWServer} from '@kaito-http/core';
 import {v1} from './routes/v1/v1.ts';
 import {getContext} from './context.ts';
+import {ZodError} from 'zod';
 
 const {server, fmw} = createFMWServer({
 	router: v1,
 	getContext,
 
 	onError: async ({error}) => {
+		if (error instanceof ZodError) {
+			return {
+				status: 400,
+				message: error.message,
+			};
+		}
+
 		return {
 			status: 500,
 			message: error.message,
