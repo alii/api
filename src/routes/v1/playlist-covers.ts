0496a1deab665aa446005f304b79ccab0a9d5390
Alistair Smith <hi@alistair.sh>
2025-01-12 11:56:00 -0500

    update

--- src/routes/v1/playlist-covers.ts
+++ src/routes/v1/playlist-covers.ts
@@ -13,19 +13,19 @@ export const playlistCovers = router().get('/', {
 	query: {
 		title: z.string().max(20),
 		image: z.string().url(),
-		size: z.string().transform(Number).pipe(z.number().positive().max(2000).default(1080)),
 	},
 	run: async ({query}) => {
+		const size = 1000;
 		const image = await Jimp.read(query.image);
 
 		const buffer = await image
-			.resize({w: query.size, h: query.size})
+			.resize({w: size, h: size})
 			.print({
 				font: avenir,
 				x: 0,
 				y: 0,
-				maxWidth: query.size,
-				maxHeight: query.size,
+				maxWidth: size,
+				maxHeight: size,
 				text: {
 					text: query.title,
 					alignmentX: HorizontalAlign.CENTER,
